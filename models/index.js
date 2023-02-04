bbe01333729d6e5409734525a665bc26769590b8
BrettSantor <b.santor27@gmail.com>
2023-02-03 17:11:22 -0800

    establish relationships

--- models/index.js
+++ models/index.js
@@ -4,17 +4,20 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+
 Product.belongsto(Category, {
-  foreignKey: 'catergory_id',
+  foreignKey: 'catergory_id'
+})
+
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
   onDelete: 'CASCADE'
 })
-// Categories have many Products
 
-// Products belongToMany Tags (through ProductTag)
 
-// Tags belongToMany Products (through ProductTag)
+Product.belongsToMany(Tag, { through: ProductTag})
 
+Tag.belongsToMany(Product, { through: ProductTag})
 module.exports = {
   Product,
   Category,
